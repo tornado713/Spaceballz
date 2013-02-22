28620a2d761472118f0851b078eb67a8e6bd690d
Michael Kidd <tornado713@gmail.com>
2013-02-21 20:04:12 -0700

    Experimenting with db files

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@
 display.setStatusBar (display.HiddenStatusBar)
 
 
-local storyboard = require "storyboard"
-local widget = require "widget"
+--local storyboard = require "storyboard"
+--local widget = require "widget"
 
 -- local monitorMem = function()
     -- collectgarbage()
@@ -14,4 +14,31 @@ local widget = require "widget"
 --Runtime:addEventListener( "enterFrame", monitorMem ) 
 
 -- load first screen
-storyboard.gotoScene( "menu" )
+--storyboard.gotoScene( "menu" )
+
+require "sqlite3"
+local path = system.pathForFile("data.db", system.ResourceDirectory)
+db = sqlite3.open(path)
+
+ --Handle the applicationExit event to close the db
+local function onSystemEvent( event )
+        if( event.type == "applicationExit" ) then              
+            db:close()
+        end
+end
+
+db:exec[[
+  create table if not exists test (id integer primary key, content);
+  insert into test values (null, 'hello world');
+  insert into test values (null, 'hello lua');
+  insert into test values (null, 'hello sqlite3')
+]]
+ 
+--print( "version " .. sqlite3.version() )
+ 
+for row in db:nrows("SELECT * FROM test") do
+  local t = display.newText(row.content, 20, 30 * row.id, null, 16)
+  t:setTextColor(255,0,255)
+end
+
+db:close()
